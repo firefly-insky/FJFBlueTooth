ef841dce30859f8fb8f1d093def4b025a6ee9f74
HubBoy <809701710@qqcom>
2016-06-14 09:34:25 +0800

    与第三方蓝牙设备通信

---
+++ FJFBluetooth/FJFBluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FJFBluetooth
+//
+//  Created by FJF on 16/6/14.
+//  Copyright © 2016年 FJF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FJFBluetooth/FJFBluetooth/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FJFBluetooth
+//
+//  Created by FJF on 16/6/14.
+//  Copyright © 2016年 FJF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
